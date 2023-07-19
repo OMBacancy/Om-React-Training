327aa096f762e661ebd7af0c6ff0888caa8a11e7
Om Mishra <om.mishra@bacancy.com>
2023-07-19 14:40:59 +0530

    Separated ExpenseDate component into a separate component

--- react-app/src/components/ExpenseItem.jsx
+++ react-app/src/components/ExpenseItem.jsx
@@ -1,13 +1,16 @@
-import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
 
-function ExpenseItem({title, amount, date}) {
-    return <div className="expense-item">
-        <div>{date}</div>
-        <div className="expense-item__description">
-            <h2>{title}</h2>
-            <div className="expense-item__price">${amount}</div>
+function ExpenseItem(props) {
+    return (
+        <div className='expense-item'>
+            <ExpenseDate date={props.date} />
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
+            </div>
         </div>
-    </div>
+    );
 }
 
 export default ExpenseItem;
