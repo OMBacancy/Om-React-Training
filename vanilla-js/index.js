2d43cbca602e0a2cd7a3716c0e8362c809bad20e
Om Mishra <om.mishra@bacancy.com>
2023-07-20 15:30:37 +0530

    var vs let observed

--- vanilla-js/index.js
+++ vanilla-js/index.js
@@ -1,11 +1,19 @@
-let a = '1'
-console.log(a) // 1 because called `a` right away
-const b = function a1() {
-    console.log(a) // 3 because a1() is innerScoped, so it searches for most recent updated value of `a` in outerScope
-    a = 2;
-    console.log(a) // 2 because after fetching from outerScope, it gets new value inside the scope
-    return a; // most updated value is returned which is 2
+// const/let vs var //
+
+if (false) {
+    let x = 1
+} else {
+    let x = 2 // cannot be accessed outside even though it is executed
 }
 
-a = '3'
-console.log(b()) // 2 because function call returned 2
+console.log(x) // throws an undefined error because let/const declare variable in lexical/inner scoping
+
+// -------------------------------------------------------- //
+
+if (false) {
+    var x = 1
+} else {
+    var x = 2
+}
+
+console.log(x) // prints 2 because var declares variable in global scoping
