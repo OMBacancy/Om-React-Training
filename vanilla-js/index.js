d8ba5766930e6914c9b2b67d925d9c1d89124df9
Om Mishra <om.mishra@bacancy.com>
2023-07-20 15:18:09 +0530

    Lexical scoping and closure concept

--- vanilla-js/index.js
+++ vanilla-js/index.js
@@ -1,8 +1,11 @@
-let outerScope = 'out'
-
-{
-    let innerScope = 'in'
+let a = '1'
+console.log(a) // 1 because called `a` right away
+const b = function a1() {
+    console.log(a) // 3 because a1() is innerScoped, so it searches for most recent updated value of `a` in outerScope
+    a = 2;
+    console.log(a) // 2 because after fetching from outerScope, it gets new value inside the scope
+    return a; // most updated value is returned which is 2
 }
 
-// innerScope variable cannot be accessed
-console.log(outerScope, " ", innerScope)
+a = '3'
+console.log(b()) // 2 because function call returned 2
