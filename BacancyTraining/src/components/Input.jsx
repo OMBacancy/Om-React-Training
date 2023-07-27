f7ea7a916f09f1ff971f4ea9e914b202d7911bcd
Om Mishra <om.mishra@bacancy.com>
2023-07-27 13:09:34 +0530

    Task 3 Completed, using useState for button adjusted disability

--- BacancyTraining/src/components/Input.jsx
+++ BacancyTraining/src/components/Input.jsx
@@ -2,20 +2,31 @@ import {useRef, useState} from "react";
 
 export default function Input() {
     const ref = useRef(null)
-    const [message, setMessage] = useState("");
+    const [buttonState, setButtonState] = useState(true);
+    const [list, setList] = useState([])
+
+    // list = []
 
     function clickHandler() {
-        setMessage(ref.current.value);
-        console.log(ref);
+        setList([ref.current.value, ...list])
+        ref.current.value = ""
+        setButtonState(true)
     }
 
-    function inputHandler() {
-        if (ref.current.value === "") setMessage("")
+    function inputFieldHandler() {
+        if (ref.current.value === "") setButtonState(true)
+        else setButtonState(false)
+
     }
 
     return <>
-        <input type="text" ref={ref}  onChange={inputHandler}/>
-        <button onClick={clickHandler}>Submit</button>
-        <h2>{message}</h2>
+        <input type="text" ref={ref} onChange={inputFieldHandler}/>
+        <button onClick={clickHandler} disabled={buttonState}>Submit</button>
+
+        <ul>
+            {
+                list.map((data, index) => <li key={index}>{data}</li>)
+            }
+        </ul>
     </>
 }
