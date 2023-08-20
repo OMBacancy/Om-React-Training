c216b5915cdba040702a8c67f19ef0e864811228
Om Mishra <om.mishra@bacancy.com>
2023-08-20 16:12:02 +0530

    re-init repository

--- BacancyTraining/src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-export default function Button(props) {
-    function eventHandler() {
-        return <h2>{props.message}</h2>
-    }
-    return <>
-        <button type={"submit"} onClick={eventHandler}>Submit</button>
-    </>
-}
-
